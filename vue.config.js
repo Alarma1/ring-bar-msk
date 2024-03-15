e8a4435d23d9a2973c840a5c51858bb6add342e6
Alarma1 <zapara03@yandex.ru>
2024-03-15 16:19:11 +0500

    поправил путь в vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/ring-bar-msk/',
+  publicPath: '/ring-bar-msk-demo/',
   transpileDependencies: true
 })
